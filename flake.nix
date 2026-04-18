5c05f71d5b576b05518f5d0028ca822a9a626315
thelissimus <thelissimus@tuta.io>
2026-04-18 22:53:06 +0500

    init

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Concrete Semantics";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [
+      "x86_64-linux"
+      "aarch64-darwin"
+    ];
+
+    perSystem = { pkgs, ... }: {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.isabelle
+        ];
+      };
+    };
+  };
+}
