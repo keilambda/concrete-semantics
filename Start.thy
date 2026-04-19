f3326f3a0876377e35111f38e353f2f43de7a524
thelissimus <thelissimus@tuta.io>
2026-04-19 05:35:32 +0500

    note(ch2.3): datatypes

--- Start.thy
+++ Start.thy
@@ -113,4 +113,21 @@ theorem gauss_sum : "sum_upto n = n * (n + 1) div 2"
   apply auto
 done
 
+type_synonym string = "char list"
+
+datatype 'a tree = Tip | Node "'a tree" 'a "'a tree"
+
+fun mirror :: "'a tree \<Rightarrow> 'a tree" where
+"mirror Tip = Tip" |
+"mirror (Node l a r) = Node (mirror r) a (mirror l)"
+
+lemma "mirror (mirror t) = t"
+  apply (induction t)
+  apply auto
+done
+
+fun lookup :: "('a * 'b) list \<Rightarrow> 'a \<Rightarrow> 'b option" where
+"lookup [] x = None" |
+"lookup ((a, b) # ps) x = (if a = x then Some b else lookup ps x)"
+
 end
